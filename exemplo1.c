1149770dd623328010a34731e9e443b5b4ba3159
Diegobataglia2024 <160801314+Diegobataglia2024@users.noreply.github.com>
2024-02-21 21:43:16 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <locale.h>
+
+int main(void) {
+  setlocale(LC_ALL, "Portuguese");
+
+  // Enunciado do exercício
+  printf("Lab 1 - Primeiro programa no replt\n");
+
+  //Variáveis
+    
+  //Programa
+  printf ("Mackenzie - FCI \n");
+  printf ("Algoritmos e ");
+  printf ("Prog. II \n");
+  printf ("Linguagem C\n");
+  return 0;
+}
